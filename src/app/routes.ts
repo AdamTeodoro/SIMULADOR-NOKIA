b20140e780ed587ab872f776b90ab33225605232
AdamTeodoro <66885644+AdamTeodoro@users.noreply.github.com>
2020-12-22 00:39:13 -0300

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,19 +4,21 @@ import { CodeController } from '../app/controllers/codeController';
 
 export const router = express.Router();
 
+//Configurando Cors
 router.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+	//Qual site tem permissão de realizar a conexão
     res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+	
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+	
     next();
 });
 
+//Recebe os valores separados do front-end e decodifica
 router.post('/decode', CodeController.decode);
 
-
-
-//Views
+//Carrega a página home
 router.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home/home.html');
 });
