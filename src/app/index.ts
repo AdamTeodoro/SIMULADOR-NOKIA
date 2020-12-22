18af390d92edd84761b0cd70c74c93ce9f6cf631
AdamTeodoro <66885644+AdamTeodoro@users.noreply.github.com>
2020-12-22 00:35:44 -0300

    Update index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,18 +5,27 @@ import { router } from './routes';
 
 class Server {
     private app = express();
-
+    
+    /**
+     * Ajusta as portas do servidor
+     */
     private normalizePort(port: number | string) {
         return port || process.env.PORT
     }
-
+    
+    /**
+     * Prepara o servidor
+     */
     private loadServer() {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(express.static('html'));
         this.app.use(router);
     }
-
+    
+    /**
+     * Inicia o servidor 
+     */
     public startServer(port: number | string) {
         this.loadServer();
 
