a71675b08ace2cc0511f2a90a212b2bc348143e5
AdamTeodoro <66885644+AdamTeodoro@users.noreply.github.com>
2020-12-22 00:32:53 -0300

    Update strmodel.ts

--- src/app/tools/strmodel.ts
+++ src/app/tools/strmodel.ts
@@ -1,12 +1,17 @@
 
 
 export class STRMODEL {
-
+    /**
+     * Transforma objeto em string
+     */
     public static toStr(json: any) {
         const str = JSON.stringify(json);
         return str;
     }
-
+    
+    /**
+     * Transforma string em objeto
+     */
     public static toJson(str: string) {
         const json = JSON.parse(str);
         return json;
